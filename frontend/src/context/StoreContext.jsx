35debba11d4cd1ecda7431cb0a12e38bdc183e15
Himanshu Sharma <himanshu.sharma2020@sait.ac.in>
2024-08-07 10:07:54 +0530

    update code

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -14,11 +14,15 @@ const StoreContextProvider = (props) => {
     const [food_list, setFoodList] = useState([])
 
 
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         } else {
             setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        }
+
+        if (token) {
+            await axios.post(`${url}/api/cart/add`, {itemId}, {headers: {token}})
         }
     }
 
